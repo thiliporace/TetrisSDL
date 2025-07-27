27123d207bb25e4a222388028048d856fa1777b7
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2025-07-27 11:48:25 -0300

    Initial Commit

---
+++ TetrisSDL/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TetrisSDL
+//
+//  Created by Thiago Liporace on 27/07/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
